07d10dfaa0faf3f8eac1fd3653e0327c2e11fc93
deepashre <86648444+deepashre@users.noreply.github.com>
2021-08-17 19:07:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
   region = var.aws_region
 }
-resource "aws_instance" "wp" { 
+resource "aws_instance" "app" { 
     ami = "ami-0e8e1023f4a053cda"
     instance_type = "t2.micro"
     key_name = "cloud-poc"
     
     tags = {
-    Name = "wp-demo"
+    Name = "Deploy-Apache"
     }
 
 }
 output "publicip" {
-  value = aws_instance.wp.public_ip
+  value = aws_instance.app.public_ip
 }
